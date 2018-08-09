fa96c4cc8c9a06892f20cfdda15e3b7b0c206590
Giovanni Paolini <giove.paolini@gmail.com>
2018-08-09 22:20:32 +0200

    Team, Player, Table.

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [
+        PlayerInline,
+    ]
+
+    list_display = ('name', 'players')
+    
+    search_fields = ('name',)
+    
+    def players(self, obj):
+        return ", ".join(player.name for player in obj.player_set.all())
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team', 'phone_number', 'is_captain')
+    # list_editable = ('team', 'phone_number', 'is_captain')
+    
+    search_fields = ('name', 'team__name')
+    autocomplete_fields = ('team',)
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'priority')
+    list_editable = ('priority',)
+
+    search_fields = ('name',)
--- tournament/models.py
+++ tournament/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=1024)
+    
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+    
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=256)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    
+    is_captain = models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        order_with_respect_to = 'team'
+
+
+
+class Table(models.Model):
+    name = models.CharField(max_length=1024)
+    priority = models.IntegerField(default=100, help_text='Tables with higher priority are preferred when creating a new turn.')
+    
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ('name',)
