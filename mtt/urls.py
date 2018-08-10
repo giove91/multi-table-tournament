072e454e94f8600ca8dba0b3c325a441085775ab
Giovanni Paolini <giove.paolini@gmail.com>
2018-08-10 16:16:46 +0200

    nonsolonumeri -> mtt

--- mtt/urls.py
+++ mtt/urls.py
@@ -1,4 +1,4 @@
-"""nonsolonumeri URL Configuration
+"""mtt URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
