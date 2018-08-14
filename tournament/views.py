8fd1090e415b822a248a621e69b12f1769ab9f2b
Giovanni Paolini <giove.paolini@gmail.com>
2018-08-14 14:01:47 +0200

    Scoreboard view.

--- tournament/views.py
+++ tournament/views.py
@@ -3,6 +3,26 @@ from django.views.generic.base import TemplateView
 
 from .models import *
 
+def sorted_scoreboard(scoreboard):
+    """
+    Take as input a scoreboard given as a counter, and return a sorted list with ranks.
+    """
+    by_score = {}
+    for (entity, score) in scoreboard.items():
+        if score not in by_score:
+            by_score[score] = []
+        by_score[score].append(entity)
+    
+    res = []
+    rank = 1
+    
+    for score in reversed(sorted(by_score)):
+        for entity in sorted(by_score[score], key=lambda x: x.name):
+            res.append((entity, scoreboard[entity], rank))
+        
+        rank += len(by_score[score])
+    
+    return res
 
 
 class IndexView(TemplateView):
@@ -13,5 +33,7 @@ class IndexView(TemplateView):
         
         context = super().get_context_data(**kwargs)
         context['tournament'] = tournament
+        
+        context['team_scoreboard'] = sorted_scoreboard(tournament.team_scoreboard())
         return context
 
